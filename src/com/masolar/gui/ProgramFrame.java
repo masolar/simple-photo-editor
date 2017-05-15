a6f29c1e71a1d5ab2e27063a20044d7dc3a19217
Masolar <mlaryins@hotmail.com>
2017-05-14 22:54:34 -0500

    Adding basic menu bar and menu items. Will clean up later and add functionality.

--- src/com/masolar/gui/ProgramFrame.java
+++ src/com/masolar/gui/ProgramFrame.java
@@ -1,13 +1,13 @@
 package com.masolar.gui;
 
-import com.masolar.gui.menu.PhotoEditorMenuBar;
+
 
 import javax.swing.*;
 
 /**
  * Created by Masolar on 5/12/2017.
  *
- * Represents the frame that will hold all elements of the
+ * Represents the frame that will hold all elements of the program.
  */
 public class ProgramFrame extends JFrame {
 
@@ -23,6 +23,9 @@ public class ProgramFrame extends JFrame {
 
         // Set up the menu bar and all menus.
         doMenuSetup();
+
+        pack();
+        setVisible(true);
     }
 
     /**
@@ -34,15 +37,35 @@ public class ProgramFrame extends JFrame {
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);
-
-        pack();
-        setVisible(true);
     }
 
     /**
      * Sets up the menu bar.
      */
     private void doMenuSetup() {
-        setJMenuBar(new PhotoEditorMenuBar());
+        JMenuBar menuBar = new JMenuBar();
+
+        // Represents the file menu
+        JMenu menu = new JMenu("File");
+        JMenuItem menuItem = new JMenuItem("New");
+        menu.add(menuItem);
+        menuBar.add(menu);
+
+        // Represents the edit menu
+        menu = new JMenu("Edit");
+
+        menuBar.add(menu);
+
+        // Represents the tool menu
+        menu = new JMenu("Tools");
+
+        menuBar.add(menu);
+
+        // Represents the window menu
+        menu = new JMenu("Window");
+
+        menuBar.add(menu);
+
+        setJMenuBar(menuBar);
     }
 }
