80ea4c38271a8292b0fe05012d69d5d76964d2b8
Masolar <mlaryins@hotmail.com>
2017-08-03 20:28:57 -0500

    Added the ability to zoom into images. Need to add margins around the picture so this is easier.

--- src/com/masolar/CustomNodes/ZoomableImageView.java
+++ src/com/masolar/CustomNodes/ZoomableImageView.java
@@ -1,15 +1,34 @@
 package com.masolar.CustomNodes;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ScrollEvent;
 
 /**
  * Created by Mason on 8/3/2017.
  */
 public class ZoomableImageView extends ImageView {
 
+    final DoubleProperty zoomProperty = new SimpleDoubleProperty(1);
     public ZoomableImageView(Image image) {
         super();
+
+        zoomProperty.addListener(observable -> {
+            setFitWidth(zoomProperty.get() * image.getWidth());
+            setFitHeight(zoomProperty.get() * image.getHeight());
+        });
+
+        addEventFilter(ScrollEvent.SCROLL, event -> {
+            if (event.isControlDown()) {
+                if (event.getDeltaY() < 0) {
+                    zoomProperty.set(zoomProperty.get() - .07);
+                } else if (event.getDeltaY() > 0) {
+                    zoomProperty.set(zoomProperty.get() + .07);
+                }
+            }
+        });
 
         setPreserveRatio(true);
         setImage(image);
